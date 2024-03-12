11098a8ed2476fb859d1b4a162df35080d34cc0e
FA00084\shark.lin <shark.lin@shoalter.com>
2024-03-12 15:12:19 +0800

    feat: init

---
+++ readme.md
@@ -0,0 +1,14 @@
+# README
+## 啟動專案三方服務
+* 前往setup資料夾
+* 執行
+```
+docker-compose up
+```
+## 關閉專案三方服務
+* 前往setup資料夾
+* 執行
+```
+docker-compose down
+```
+## Swagger UI
--- src/main/java/org/shark/example/dao/repository/WorkHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.shark.example.dao.repository;
-
-import org.shark.example.dao.repository.pojo.WorkHistoryDo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkHistoryRepository extends JpaRepository<WorkHistoryDo, Integer> {
-}
