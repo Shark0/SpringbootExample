2cf99102724ce0319116a09eb3ed533fdbeb735e
TW - IT - Shark <shark.lin@shoalter.com>
2026-02-09 13:39:59 +0800

    feat: change java to kotlin

---
+++ src/main/kotlin/org/shark/example/controller/i18n/I18nController.kt
@@ -0,0 +1,20 @@
+package org.shark.example.controller.i18n
+
+import org.shark.example.service.base.pojo.ResponseDto
+import org.shark.example.service.i18n.GetI18nService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/i18n")
+class I18nController(
+    private val getI18nService: GetI18nService
+) {
+
+    @GetMapping
+    fun getMessage(@RequestParam value1: String, @RequestParam value2: Int): ResponseDto<String> {
+        return getI18nService.start(value1, value2)
+    }
+}
---
+++ src/main/kotlin/org/shark/example/service/i18n/pojo/GetMessageDto.kt
@@ -0,0 +1,6 @@
+package org.shark.example.service.i18n.pojo
+
+data class GetMessageDto(
+    val value1: String? = null,
+    val value2: String? = null
+)
